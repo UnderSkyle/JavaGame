0e17cbaefa51d1b1af39e0b526f2bbc8299e9187
Simon <landrysimo@cy-tech.fr>
2024-04-30 13:33:02 +0200

    added tp to center if touch boundary

--- src/main/java/com/example/demo/BasicGameApp.java
+++ src/main/java/com/example/demo/BasicGameApp.java
@@ -5,8 +5,11 @@ package com.example.demo;/*
  */
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.Viewport;
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -16,65 +19,75 @@ import javafx.scene.text.Text;
 import java.util.Map;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
+import static java.lang.Math.abs;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 public class BasicGameApp extends GameApplication {
+
+
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(600);
-        settings.setHeight(600);
-        settings.setTitle("Basic Game App");
-        settings.setVersion("0.1");
+        settings.setWidth(800);
+        settings.setHeight(800);
+        settings.setTitle("Potato Game");
+        settings.setVersion("0.2");
     }
 
     public enum EntityType {
-        PLAYER, COIN
+        PLAYER, COIN, WALL
     }
 
     @Override
     protected void initInput() {
         onKey(KeyCode.D, () -> {
             player.translateX(5); // move right 5 pixels
-            inc("pixelsMoved", +5);
         });
 
         onKey(KeyCode.Q, () -> {
             player.translateX(-5); // move left 5 pixels
-            inc("pixelsMoved", -5);
         });
 
         onKey(KeyCode.Z, () -> {
             player.translateY(-5); // move up 5 pixels
-            inc("pixelsMoved", +5);
         });
 
         onKey(KeyCode.S, () -> {
             player.translateY(5); // move down 5 pixels
-            inc("pixelsMoved", -5);
         });
 
         onKeyDown(KeyCode.F, () -> {
-            play("drop.wav");
+            System.out.println(player.getX() + " " + player.getY());
         });
     }
 
     @Override
     protected void initGameVars(Map<String, Object> vars) {
-        vars.put("pixelsMoved", 0);
+        vars.put("PositionX", 300);
+        vars.put("PositionY", 300);
     }
 
     private Entity player;
 
     @Override
     protected void initGame() {
+        getGameScene().setBackgroundColor(Color.BLACK);;
+        entityBuilder()
+                .viewWithBBox(new Rectangle(getAppWidth(), getAppHeight(), Color.BEIGE))
+                .zIndex(-1)
+                .buildAndAttach();
+
         player = entityBuilder()
                 .type(EntityType.PLAYER)
                 .at(300, 300)
-                .viewWithBBox(new Rectangle(40, 40, Color.BLUE))
+                .viewWithBBox(new Rectangle(20, 20, Color.BLUE))
                 .with(new CollidableComponent(true))
                 .buildAndAttach();
+
+        player.setProperty("prevX", player.getX());
+        player.setProperty("prevY", player.getY());
+
 
         entityBuilder()
                 .type(EntityType.COIN)
@@ -82,6 +95,15 @@ public class BasicGameApp extends GameApplication {
                 .viewWithBBox(new Circle(15, 15, 15, Color.YELLOW))
                 .with(new CollidableComponent(true))
                 .buildAndAttach();
+
+        initScreenBounds();
+
+        Viewport viewport = FXGL.getGameScene().getViewport();
+        viewport.bindToEntity(player, getAppWidth() / 2.0, getAppHeight() / 2.0);
+        viewport.setBounds(0, 0, getAppWidth(), FXGL.getAppHeight());
+        viewport.bindToEntity(player, FXGL.getAppWidth() / 2.0, FXGL.getAppHeight() / 2.0);
+        viewport.setLazy(true);
+
     }
 
     @Override
@@ -89,23 +111,43 @@ public class BasicGameApp extends GameApplication {
         // order of types on the right is the same as on the left
         onCollisionBegin(EntityType.PLAYER, EntityType.COIN, (player, coin) -> {
             coin.removeFromWorld();
+
         });
+
+        onCollisionBegin(EntityType.PLAYER, EntityType.WALL, (player, boundary) -> {
+            /*Point2D center = boundary.getCenter();
+            double dx = center.getX() - player.getX();
+            double dy = center.getY() - player.getY();
+            System.out.println(dx);
+            System.out.println(dy);
+            if (dx > 395) {
+                //set center.X to something else
+                player.translateX(15);
+
+            }
+            else if ( dy > 395) {
+                //set center.Y to something else
+                player.translateY(15);
+            }
+            else if ( dx > -395 && dx < -370) {
+                player.translateX(-15);
+            }
+            else if (dy > -395 && dy < -370) {
+                player.translateY(-15);
+
+            }*/
+            player.setPosition(boundary.getCenter());
+
+        });
+
     }
+    private void initScreenBounds(){
+        Entity walls = entityBuilder()
+                .type(EntityType.WALL)
+                .collidable()
+                .buildScreenBounds(50);
 
-    @Override
-    protected void initUI() {
-        Text textPixels = new Text();
-        textPixels.setTranslateX(50); // x = 50
-        textPixels.setTranslateY(100); // y = 100
-        textPixels.textProperty().bind(getip("pixelsMoved").asString());
-
-        addUINode(textPixels); // add to the scene graph
-
-        var brickTexture = texture("brick.png");
-        brickTexture.setTranslateX(50);
-        brickTexture.setTranslateY(450);
-
-        addUINode(brickTexture);
+        getGameWorld().addEntity(walls);
     }
 
     public static void main(String[] args) {
